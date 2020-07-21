cff18543f41ee877a15fd2d9bcf0449afc4dc874
AndrezaEmiliano <andreza.emiliano@exactaworks.com.br>
2020-07-21 14:05:52 -0300

    Creating ProductCategory Class

---
+++ src/ProductCategory.java
@@ -0,0 +1,21 @@
+public class ProductCategory {
+
+    private String category;
+
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String category) {
+        this.category = category;
+    }
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
