2d313092b80c3bd4a3f5cca9fb01b202ab7381a0
Alexandre Wiggert <alexandrewiggert@gmail.com>
2020-09-03 11:19:43 -0300

    Pagination and sorting with Spring Data

--- src/main/java/br/com/exactalabs/bicycleshop/BicycleShopApplication.java
+++ src/main/java/br/com/exactalabs/bicycleshop/BicycleShopApplication.java
@@ -1,11 +1,17 @@
 package br.com.exactalabs.bicycleshop;
 
+import br.com.exactalabs.bicycleshop.entity.Task;
 import br.com.exactalabs.bicycleshop.repository.TaskRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BicycleShopApplication {
@@ -20,8 +26,33 @@ public class BicycleShopApplication {
             //Encarem como um construtor
             TaskRepository taskRepository = appContext.getBean(TaskRepository.class);
 
-            System.out.println("Tasks que possuam subtasks pendentes:");
-            System.out.println(taskRepository.findBySubtasksDone(false));
+            PageRequest page = PageRequest.of(0, 2);
+
+            PageRequest pageWithSort = PageRequest.of(0, 2,
+                    Sort.by("name")
+                            .and(Sort.by("done"))
+                            .ascending());
+
+            Page<Task> tasksPaginadas = taskRepository.findByDone(false, pageWithSort);
+
+            int numeroPagina = tasksPaginadas.getNumber() + 1;
+
+            System.out.println("Total de tasks: " + tasksPaginadas.getTotalElements());
+            System.out.println("Total de paginas: " + tasksPaginadas.getTotalPages());
+            System.out.println("Numero da pagina: " + numeroPagina);
+            System.out.println("Tasks: " + tasksPaginadas.getContent());
+
+            System.out.println(tasksPaginadas);
+
+            List<Task> tasksOrdenadasPorNome =
+                    taskRepository.findAll(Sort.by("name"));
+
+            System.out.println(tasksOrdenadasPorNome);
+
+            List<Task> tasksPendentesOrdenadas =
+                    taskRepository.findByDone(false, Sort.by("name"));
+
+            System.out.println(tasksPendentesOrdenadas);
 
         };
     }
--- src/main/java/br/com/exactalabs/bicycleshop/repository/TaskRepository.java
+++ src/main/java/br/com/exactalabs/bicycleshop/repository/TaskRepository.java
@@ -1,6 +1,10 @@
 package br.com.exactalabs.bicycleshop.repository;
 
 import br.com.exactalabs.bicycleshop.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,9 +14,11 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    List<Task> findByDone(boolean done);
+    List<Task> findByDone(boolean done, Sort sort);
 
-    List<Task> findByNameLikeOrderByNameAsc(String name);
+    Page<Task> findByDone(boolean done, Pageable pageRequest);
+
+    Page<Task> findByNameLikeOrderByNameAsc(String name, Pageable pageRequest);
 
     @Query("SELECT t.name FROM Task t")
     List<String> findAllTasksName();
