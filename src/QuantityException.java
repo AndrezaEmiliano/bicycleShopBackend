db4fcf1bb7f7c1e244c1039ad63972d45e7cbb78
AndrezaEmiliano <andreza.emiliano@exactaworks.com.br>
2020-09-30 20:32:45 -0300

    settings

--- src/QuantityException.java
+++
@@ -1,5 +0,0 @@
-public class QuantityException extends RuntimeException{
-    public QuantityException(String msg) {
-        super (msg);
-    }
-}
--- src/main/java/br/com/exactalabs/bicycleshop/entity/PriceException.java
+++ src/main/java/br/com/exactalabs/bicycleshop/entity/PriceException.java
@@ -1,3 +1,5 @@
+package br.com.exactalabs.bicycleshop.entity;
+
 public class PriceException extends RuntimeException {
     public PriceException (String msg) {
         super (msg);
