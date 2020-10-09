9394d8f9fb0c80a8518da1a51c594a987fb77c7f
AndrezaEmiliano <andreza.emiliano@exactaworks.com.br>
2020-10-09 12:42:40 -0300

    Adding classes BankSlip, CreditCard, Order, PaymentMethod

---
+++ src/main/java/br/com/exactalabs/bicycleshop/entity/CreditCard.java
@@ -0,0 +1,73 @@
+package br.com.exactalabs.bicycleshop.entity;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CreditCard extends PaymentMethod {
+
+    private String cardNumber;
+    private LocalDate expDate;
+    private int cvv;
+    private String cardName;
+    private Integer flexPaymentNumber;
+
+
+    public CreditCard() {
+    }
+
+    public CreditCard(String cardNumber, LocalDate expDate, int cvv, String cardName, Integer flexPaymentNumber) {
+        this.cardNumber = cardNumber;
+        this.expDate = expDate;
+        this.cvv = cvv;
+        this.cardName = cardName;
+        this.flexPaymentNumber = flexPaymentNumber;
+    }
+
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public LocalDate getExpDate() {
+        return expDate;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public Integer getFlexPaymentNumber() {
+        return flexPaymentNumber;
+    }
+
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void setExpDate(LocalDate expDate) throws ExpDateException{
+        if (expDate == null){
+            throw new ExpDateException("O cartão não pode ter a data de validade nula.");
+        }
+        if (expDate.isBefore(LocalDate.now())){
+            throw new ExpDateException("O cartão está com a validade expirada");
+        }
+        this.expDate = expDate;
+    }
+
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public void setFlexPaymentNumber(Integer flexPaymentNumber) {
+        this.flexPaymentNumber = flexPaymentNumber;
+    }
+}
